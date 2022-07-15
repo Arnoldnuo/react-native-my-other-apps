5a641332112e9a7734a9be347d4100377d7939f3
linuo <nuoli@sfmail.sf-express.com>
2022-07-15 16:29:35 +0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ListItem, Avatar, Button } from "@rneui/themed";
 import { Linking, Platform, View } from "react-native";
 
